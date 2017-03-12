b0b443637cb4a34848e40861e4daae5afa471dfd
Fiona <h.natapra@gmail.com>
2017-03-12 13:44:45 -0400

    show controlla

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 
 let vendor = [
-  {name: "Cest la Pommes", city: "Bayville", state: "VA", website: "http://www.apple.com/" , image: "http://cdn.rosannadavisonnutrition.com/wp-content/uploads/2015/12/apples.jpeg"  }
+  {name: "Cest la Pommes", city: "Bayville", state: "VA", website: "http://www.apple.com/" , image: "http://cdn.rosannadavisonnutrition.com/wp-content/uploads/2015/12/apples.jpeg"  },
+  {name: "Cest la Apples", city: "Bayville", state: "VA", website: "http://www.apple.com/" , image: "http://cdn.rosannadavisonnutrition.com/wp-content/uploads/2015/12/apples.jpeg"  }
 ]
 
-
+ // need to add FarmartFactoryController once RAILS API is ready
 angular
   .module("farmart", [
     "ui.router",
@@ -13,10 +14,15 @@ angular
   "$stateProvider",
   RouterFunction
   ])
-
   .controller("FarmartIndexController", [
     FarmartIndexControllerFunction
   ])
+  .controller("FarmartShowController", [
+    "$stateParams",
+    FarmartIndexControllerFunction
+  ])
+
+
 
 
 function RouterFunction($stateProvider) {
@@ -27,8 +33,13 @@ function RouterFunction($stateProvider) {
     controller: "FarmartIndexController",
     controllerAs: "vm"
   })
+  .state("farmartShow", {
+    url: "/farmart/:id",
+    templateUrl: "js/ng-views/show.html",
+    controller: "FarmartShowController",
+    controllerAs: "vm"
+  })
   }
-
 
 // function FarmartFactoryFunction($resource){
 //   return $resource()
@@ -36,4 +47,8 @@ function RouterFunction($stateProvider) {
 
 function FarmartIndexControllerFunction() {
   this.vendors = vendor
+}
+
+function FarmartShowControllerFunction($stateParams) {
+  this.vendor = vendor.get({id: $stateParams.id});
 }
